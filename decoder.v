786e4f755d2f96fd54044f05c06a9a1e04a04d68
anushadatar <anushadatar@gmail.com>
2018-09-20 21:35:14 -0400

    finished decoder

--- decoder.v
+++ decoder.v
@@ -1,4 +1,7 @@
-// Decoder circuit
+// Define operations.
+`define AND and #50
+`define OR or #50
+`define NOT not #50
 
 module behavioralDecoder
 (
@@ -17,6 +20,21 @@ module structuralDecoder
     input address0, address1,
     input enable
 );
-    // Your decoder code here
+    // e = enabled, n = not, ne = not enabled.
+    wire ea0, ea1, na0, na1, nea0, nea1;
+    
+    // Prep transition steps.
+    `AND ande0(ea0, address0, enable);
+    `AND ande1(ea1, address1, enable);
+    `NOT not0(na0, address0);
+    `NOT not1(na1, address1);
+    `AND and0(nea0, na0, enable);
+    `AND and1(nea1, na1, enable);
+
+    // AND necessary pieces for outputs.
+    `AND and2(out0, nea0, nea1);
+    `AND and3(out1, ea0, nea1);
+    `AND and4(out2, nea0, ea1);
+    `AND and5(out3, ea0, ea1);  
 endmodule
 
